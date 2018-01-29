3812d4432c22afe3d0a5a0579db13150ee363d77
premsrepo <premgowdabp@gmail.com>
2018-01-29 14:40:53 +0530

    project setup with child component

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <logo size="200"></logo>
+    </div>
+</template>
+
+<script>
+import logo from '../components/logo'
+
+export default {
+  name: 'login',
+  components: { logo },
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$store.dispatch('login', {
+        email: this.email,
+        password: this.password
+      }).then(() => {
+        this.$router.push('/objective/add')
+      })
+    }
+  }
+}
+</script>
---
+++ src/components/Logo.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div>
+    <h1>Logo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'logo',
+  props: {
+    size: {
+      default: 80
+    }
+  },
+  data: function () {
+    return {
+      logo_size: this.size
+    }
+  }
+}
+</script>
